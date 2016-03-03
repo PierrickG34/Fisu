3307231dfad8fb74bf7541d1fd336ca019db723a
kevin margueritte <kevinmargueritte@macbook-air-de-kevin.local>
2016-03-03 22:40:44 +0100

    Initial Commit

---
+++ fisu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fisu
+//
+//  Created by kevin margueritte on 03/03/2016.
+//  Copyright © 2016 kevin margueritte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
